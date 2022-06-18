613b9c0934d5a170798974a217471025134cc4f6
peteS-UK <peter@sketch.cc>
2022-06-18 09:43:45 +0100

    Install  update for renaming

--- SetupOLED4.sh
+++ SetupOLED4.sh
@@ -18,32 +18,36 @@ tmpdir=$(mktemp -d)
 echo "Installing python3 and freetype extension"
 tce-load -iw python3.8 freetype 1>>/dev/null 2>>/dev/null
 
-echo "Downloading Extension from GitHub"
-wget -q https://github.com/peteS-UK/EvoSabre-DAC-PCP/releases/download/pcpOLED/pcpOLED4.tar.gz -O $tmp
+echo "Downloading extension from GitHub"
+wget -q https://github.com/peteS-UK/EvoSabre-DAC-PCP/releases/download/oled4pcp/oled4pcp_4.tar.gz -O $tmp
 
 echo "Unpacking Files"
 tar -xzf $tmp -C $tmpdir
 
 rm $tmp
 
-echo "Moving Files to home"
-
-mv -f $tmpdir/pcpoled4/home/* ~
-
-#mv -f $tmpdir/pcpoled4/home/logo.bmp ~
-
 mkdir ~/fonts 2>>/dev/null
-mv -f $tmpdir/pcpoled4/home/fonts/* ~/fonts
+
+echo "Moving Files to home"
+wget -q https://raw.githubusercontent.com/peteS-UK/EvoSabre-DAC-PCP/main/home/oled4pcp_4.py -P ~
+wget -q https://raw.githubusercontent.com/peteS-UK/EvoSabre-DAC-PCP/main/home/helper.py -P ~
+wget -q https://raw.githubusercontent.com/peteS-UK/EvoSabre-DAC-PCP/main/home/oled4pcp.cfg -P ~
+wget -q https://raw.githubusercontent.com/peteS-UK/EvoSabre-DAC-PCP/main/home/logo_256_64.bmp -P ~
+wget -q https://raw.githubusercontent.com/peteS-UK/EvoSabre-DAC-PCP/main/home/logo_128_64.bmp -P ~
+wget -q https://raw.githubusercontent.com/peteS-UK/EvoSabre-DAC-PCP/main/home/fonts/arial.ttf -P ~/fonts
+wget -q https://raw.githubusercontent.com/peteS-UK/EvoSabre-DAC-PCP/main/home/fonts/fontawesome-webfont.ttf -P ~/fonts
+wget -q https://raw.githubusercontent.com/peteS-UK/EvoSabre-DAC-PCP/main/home/fonts/msyh.ttf -P ~/fonts
+
 
 if [ "$(uname -m)" = "aarch64" ]; then
     echo "Installing 64 bit extension"
-    tczname="pcpoled4-py38-64-deps.tcz"
+    tczname="oled4pcp_4-py38-64-deps.tcz"
 else
     echo "Installing 32 bit extension"
-    tczname="pcpoled4-py38-deps.tcz"
+    tczname="oled4pcp_4-py38-deps.tcz"
 fi
 
-sudo cp -p "$tmpdir/pcpoled4/$tczname" /etc/sysconfig/tcedir/optional 1>>/dev/null
+sudo cp -p "$tmpdir/oled4pcp_4/$tczname" /etc/sysconfig/tcedir/optional 1>>/dev/null
 echo "$tczname" | sudo tee -a /etc/sysconfig/tcedir/onboot.lst 1>>/dev/null
 
 rm -rf $tmpdir
@@ -54,11 +58,11 @@ while read line; do
         if [ $? -eq 0 ]; then
         	section=$(echo $line)
         fi
-done < ~/oled.cfg
+done < ~/oled4pcp.cfg
 
 if [ ${#section} = 0 ]; then
-    echo "oled.cfg file contains no section for $oleddevice"
-    echo "Please edit oled.cfg to define your oled device, and backup and reboot after changes."
+    echo "oled4pcp.cfg file contains no section for $oleddevice"
+    echo "Please edit oled4pcp.cfg to define your oled device, and backup and reboot after changes."
 fi
 
 #Check if USER_COMMAND_1 is set already
@@ -74,7 +78,7 @@ UC_LINE=$(echo $UC1 | awk -F'USER_COMMAND_1=' '{print $2}' | sed 's/"//g')
 if [ "$UC_LINE" == "" ]; then
     # Command line is blank, so update it
     echo "Updating User Command"
-    $(sed -i "s/USER_COMMAND_1=\"\"/USER_COMMAND_1=\"python3+%2Fhome%2Ftc%2Flms_oled_4.py+OLED%3D$oleddevice\"/" /usr/local/etc/pcp/pcp.cfg)
+    $(sed -i "s/USER_COMMAND_1=\"\"/USER_COMMAND_1=\"python3+%2Fhome%2Ftc%2Foled4pcp_4.py+OLED%3D$oleddevice\"/" /usr/local/etc/pcp/pcp.cfg)
 fi
 
 while true; do
